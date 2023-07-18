d243e743d37579792f87c74fa1ab8b8047eafa54
Irazu Serrano <jani.serrano303@gmail.com>
2023-07-18 05:27:01 -0600

    second commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// comentario
+
+var app = document.getElementById("app");
+
+var typewriter = new Typewriter(app, {
+  loop: false,
+  delay: 75,
+});
+
+typewriter
+  .pauseFor(2500)
+  .typeString("Bienvenidas y bienvenidos a esta página web.")
+  .pauseFor(300)
+  .deleteChars(32)
+  .typeString('<strong><span style="color: purple;"> Techolochicas </span></strong>')
+  .typeString(
+    '<strong><span style="color: brown;"> ¡Saludos Cordiales! </span></strong>'
+  )
+  .pauseFor(1000)
+  .start();
